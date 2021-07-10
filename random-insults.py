c30c09db5348445e6e2664abb152e0ccb8a13dd5
haukur <72574515+HaukurLogi@users.noreply.github.com>
2021-07-10 14:25:41 +0000

    Update random-insults.py
    edis

--- random-insults.py
+++ random-insults.py
@@ -1,26 +1,26 @@
-maria = input(" Nafn: ")
+name = input(" Nafn: ")
 import random
 n = random.randint(1,11)
 
 if n == 1:
-    print("Halló,", maria, "Þú lítur vel út í dag.")
+    print("Halló,", name, "Þú lítur vel út í dag.")
 if n == 2:
-    print("Halló,", maria, "Hárið þitt lítur út eins og fugla hreiður.")
+    print("Halló,", name, "Hárið þitt lítur út eins og fugla hreiður.")
 if n == 3:
-    print("Halló,", maria, "Þú ert rosalega ung/ur.")
+    print("Halló,", name, "Þú ert rosalega ung/ur.")
 if n == 4:
-    print("Halló,", maria, "Þú ert rosalega gömul/gamall.")
+    print("Halló,", name, "Þú ert rosalega gömul/gamall.")
 if n == 5:
-    print("Halló,", maria, "Meiddiru þig þegar þú dast frá himininum vegna þess þú ert engill.")
+    print("Halló,", name, "Meiddiru þig þegar þú dast frá himininum vegna þess þú ert engill.")
 if n == 6:
-    print("Halló,", maria, "Tonlistarsmekkurinn þinn er verri en þetta forrit.")
+    print("Halló,", name, "Tonlistarsmekkurinn þinn er verri en þetta forrit.")
 if n == 7:
-    print("Halló,", maria, "Þú ert ekki eitt að uppáhalds nammið hanns pabba.")
+    print("Halló,", name, "Þú ert ekki eitt að uppáhalds nammið hanns pabba.")
 if n == 8:
-    print("Halló,", maria, "Þú ert frábær.")
+    print("Halló,", name, "Þú ert frábær.")
 if n == 9:
-    print("Halló,", maria, "nefið þitt er skrýtið.")
+    print("Halló,", name, "nefið þitt er skrýtið.")
 if n == 10:
-    print("Halló,", maria, "þú ert sætur strumpur.")
+    print("Halló,", name, "þú ert sætur strumpur.")
 if n == 11:
-    print("Halló", maria, "strumpa nef nef nef hahahahahahah lolololol.")
+    print("Halló", name, "strumpa nef nef nef hahahahahahah lolololol.")
