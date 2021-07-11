fdb929c9cf0675645779800fcbf3b7dbe8c8d773
haukur <72574515+HaukurLogi@users.noreply.github.com>
2021-07-11 19:58:58 +0000

    Create prestur.py
    ef þú veist ekki hvað á að skýra barnið þitt

---
+++ prestur.py
@@ -0,0 +1,414 @@
+kyn = input(" (Litlir Stafir) Kyn: ")
+
+if kyn != ("strákur") and kyn != ("stelpa"):
+    print(" Það er ekki kyn. ")
+
+import random
+n = random.randint(1,50)
+n1 = random.randint(51,100) 
+n2 = random.randint(101,150)
+n3 = random.randint(151,200)
+
+if kyn == ("stelpa") and n == 1:
+    print("Anna")
+if kyn == ("stelpa") and n == 2:
+    print("Máney")
+if kyn == ("stelpa") and n == 3:
+    print("Eva")
+if kyn == ("stelpa") and n == 4:
+    print("Emilía")
+if kyn == ("stelpa") and n == 5:
+    print("Ásdís")
+if kyn == ("stelpa") and n == 6:
+    print("María")
+if kyn == ("stelpa") and n == 7:
+    print("Birna")
+if kyn == ("stelpa") and n == 8:
+    print("Fjóla")
+if kyn == ("stelpa") and n == 9:
+    print("Kristín")
+if kyn == ("stelpa") and n == 10:
+    print("Bergþóra")
+if kyn == ("stelpa") and n == 11:
+    print("Jónína")
+if kyn == ("stelpa") and n == 12:
+    print("Þula")
+if kyn == ("stelpa") and n == 13:
+    print("Llósey")
+if kyn == ("stelpa") and n == 14:
+    print("Blær")
+if kyn == ("stelpa") and n == 15:
+    print("Ingibjörg")
+if kyn == ("stelpa") and n == 16:
+    print("Inga")
+if kyn == ("stelpa") and n == 17:
+    print("Sigrún")
+if kyn == ("stelpa") and n == 18:
+    print("Ísabella")
+if kyn == ("stelpa") and n == 19:
+    print("Aagot")
+if kyn == ("stelpa") and n == 20:
+    print("Adda")
+if kyn == ("stelpa") and n == 21:
+    print("Dagný")
+if kyn == ("stelpa") and n == 22:
+    print("Arndís")
+if kyn == ("stelpa") and n == 23:
+    print("Arnlaug")
+if kyn == ("stelpa") and n == 24:
+    print("Baldey")
+if kyn == ("stelpa") and n == 25:
+    print("Begga")
+if kyn == ("stelpa") and n == 26:
+    print("Benidikta")
+if kyn == ("stelpa") and n == 27:
+    print("Berglín")
+if kyn == ("stelpa") and n == 28:
+    print("Bjarkey")
+if kyn == ("stelpa") and n == 29:
+    print("Bjarngerður")
+if kyn == ("stelpa") and n == 30:
+    print("Karen")
+if kyn == ("stelpa") and n == 31:
+    print("Kara")
+if kyn == ("stelpa") and n == 32:
+    print("Kata")
+if kyn == ("stelpa") and n == 33:
+    print("Kleópatra")
+if kyn == ("stelpa") and n == 34:
+    print("Rafney")
+if kyn == ("stelpa") and n == 35:
+    print("Rósa")
+if kyn == ("stelpa") and n == 36:
+    print("Rósey")
+if kyn == ("stelpa") and n == 37:
+    print("Zoe")
+if kyn == ("stelpa") and n == 38:
+    print("Auður")
+if kyn == ("stelpa") and n == 39:
+    print("Ýr")
+if kyn == ("stelpa") and n == 40:
+    print("Þjóðhildur")
+if kyn == ("stelpa") and n == 41:
+    print("Saga")
+if kyn == ("stelpa") and n == 42:
+    print("Salka")
+if kyn == ("stelpa") and n == 43:
+    print("Magna")
+if kyn == ("stelpa") and n == 44:
+    print("Kría")
+if kyn == ("stelpa") and n == 45:
+    print("Kristel")
+if kyn == ("stelpa") and n == 46:
+    print("Hadda")
+if kyn == ("stelpa") and n == 47:
+    print("Heiða")
+if kyn == ("stelpa") and n == 48:
+    print("Hendrikka")
+if kyn == ("stelpa") and n == 49:
+    print("Jónsa")
+if kyn == ("stelpa") and n == 50:
+    print("Hera")
+
+if kyn == ("stelpa") and n1 == 51:
+    print("Þula")
+if kyn == ("stelpa") and n1 == 52:
+    print("Ýr")
+if kyn == ("stelpa") and n1 == 53:
+    print("Kristín")
+if kyn == ("stelpa") and n1 == 54:
+    print("Aðaldal")
+if kyn == ("stelpa") and n1 == 55:
+    print("Aldan")
+if kyn == ("stelpa") and n1 == 56:
+    print("Bald")
+if kyn == ("stelpa") and n1 == 57:
+    print("Eva")
+if kyn == ("stelpa") and n1 == 58:
+    print("Kaldal")
+if kyn == ("stelpa") and n1 == 59:
+    print("Sól")
+if kyn == ("stelpa") and n1 == 60:
+    print("Björt")
+if kyn == ("stelpa") and n1 == 61:
+    print("Áss")
+if kyn == ("stelpa") and n1 == 62:
+    print("Ást")
+if kyn == ("stelpa") and n1 == 63:
+    print("Sunna")
+if kyn == ("stelpa") and n1 == 64:
+    print("Aðalveig")
+if kyn == ("stelpa") and n1 == 65:
+    print("Angelía")
+if kyn == ("stelpa") and n1 == 66:
+    print("Arnóra")
+if kyn == ("stelpa") and n1 == 67:
+    print("Ásla")
+if kyn == ("stelpa") and n1 == 68:
+    print("Ósk")
+if kyn == ("stelpa") and n1 == 69:
+    print("Rós")
+if kyn == ("stelpa") and n1 == 70:
+    print("Dís")
+if kyn == ("stelpa") and n1 == 71:
+    print("Sandra")
+if kyn == ("stelpa") and n1 == 72:
+    print("Dögg")
+if kyn == ("stelpa") and n1 == 73:
+    print("Sigurást")
+if kyn == ("stelpa") and n1 == 74:
+    print("Anna")
+if kyn == ("stelpa") and n1 == 75:
+    print("Lea")
+if kyn == ("stelpa") and n1 == 76:
+    print("Lind")
+if kyn == ("stelpa") and n1 == 77:
+    print("Lilja")
+if kyn == ("stelpa") and n1 == 78:
+    print("Björg")
+if kyn == ("stelpa") and n1 == 79:
+    print("Björk")
+if kyn == ("stelpa") and n1 == 80:
+    print("Karen")
+if kyn == ("stelpa") and n1 == 81:
+    print("Birta")
+if kyn == ("stelpa") and n1 == 82:
+    print("Brá")
+if kyn == ("stelpa") and n1 == 83:
+    print("Sara")
+if kyn == ("stelpa") and n1 == 84:
+    print("Sif")
+if kyn == ("stelpa") and n1 == 85:
+    print("Hrund")
+if kyn == ("stelpa") and n1 == 86:
+    print("Sjöfn")
+if kyn == ("stelpa") and n1 == 87:
+    print("María")
+if kyn == ("stelpa") and n1 == 88:
+    print("Von")
+if kyn == ("stelpa") and n1 == 89:
+    print("Lóa")
+if kyn == ("stelpa") and n1 == 90:
+    print("Nótt")
+if kyn == ("stelpa") and n1 == 91:
+    print("Júlía")
+if kyn == ("stelpa") and n1 == 92:
+    print("Tómatsósa")
+if kyn == ("stelpa") and n1 == 93:
+    print("Gaffall")
+if kyn == ("stelpa") and n1 == 94:
+    print("Mía")
+if kyn == ("stelpa") and n1 == 95:
+    print("Sólbjört")
+if kyn == ("stelpa") and n1 == 96:
+    print("Inga")
+if kyn == ("stelpa") and n1 == 97:
+    print("Lóa")
+if kyn == ("stelpa") and n1 == 98:
+    print("Laufblað")
+if kyn == ("stelpa") and n1 == 99:
+    print("Loga")
+if kyn == ("stelpa") and n1 == 100:
+    print("Náttsól")
+
+if kyn == ("strákur") and n2 == 101:
+    print ("Haukur")
+if kyn == ("strákur") and n2 == 102:
+    print ("Guðmundur")
+if kyn == ("strákur") and n2 == 103:
+    print ("Árni")
+if kyn == ("strákur") and n2 == 104:
+    print ("Birnir")
+if kyn == ("strákur") and n2 == 105:
+    print ("Kristófer")
+if kyn == ("strákur") and n2 == 106:
+    print ("Snorri")
+if kyn == ("strákur") and n2 == 107:
+    print ("Loftur")
+if kyn == ("strákur") and n2 == 108:
+    print ("Sturla")
+if kyn == ("strákur") and n2 == 109:
+    print ("Garðar")
+if kyn == ("strákur") and n2 == 110:
+    print ("Jón")
+if kyn == ("strákur") and n2 == 111:
+    print ("Gunnar")
+if kyn == ("strákur") and n2 == 112:
+    print ("Björn")
+if kyn == ("strákur") and n2 == 113:
+    print ("Pétur")
+if kyn == ("strákur") and n2 == 114:
+    print ("Sturla")
+if kyn == ("strákur") and n2 == 115:
+    print ("Oddur")
+if kyn == ("strákur") and n2 == 116:
+    print ("Ingólfur")
+if kyn == ("strákur") and n2 == 117:
+    print ("Benidikt")
+if kyn == ("strákur") and n2 == 118:
+    print ("Róbert")
+if kyn == ("strákur") and n2 == 119:
+    print ("Viktor")
+if kyn == ("strákur") and n2 == 120:
+    print ("Hlynur")
+if kyn == ("strákur") and n2 == 121:
+    print ("Þorkell")
+if kyn == ("strákur") and n2 == 122:
+    print ("Arnar")
+if kyn == ("strákur") and n2 == 123:
+    print ("Kristján")
+if kyn == ("strákur") and n2 == 124:
+    print ("Bjartur")
+if kyn == ("strákur") and n2 == 125:
+    print ("Baldur")
+if kyn == ("strákur") and n2 == 126:
+    print ("Kjartan")
+if kyn == ("strákur") and n2 == 127:
+    print ("Logi")
+if kyn == ("strákur") and n2 == 128:
+    print ("Dagur")
+if kyn == ("strákur") and n2 == 129:
+    print ("Ríkarður")
+if kyn == ("strákur") and n2 == 130:
+    print ("Helgi")
+if kyn == ("strákur") and n2 == 131:
+    print ("Ólafur")
+if kyn == ("strákur") and n2 == 132:
+    print ("Eiríkur")
+if kyn == ("strákur") and n2 == 133:
+    print ("Bjarmi")
+if kyn == ("strákur") and n2 == 134:
+    print ("Gauti")
+if kyn == ("strákur") and n2 == 135:
+    print ("Línus")
+if kyn == ("strákur") and n2 == 136:
+    print ("Magnús")
+if kyn == ("strákur") and n2 == 137:
+    print ("Sigurður")
+if kyn == ("strákur") and n2 == 138:
+    print ("Kalli")
+if kyn == ("strákur") and n2 == 139:
+    print ("Guðjón")
+if kyn == ("strákur") and n2 == 140:
+    print ("Guðfinnur")
+if kyn == ("strákur") and n2 == 141:
+    print ("Adólf")
+if kyn == ("strákur") and n2 == 142:
+    print ("Hektor")
+if kyn == ("strákur") and n2 == 143:
+    print ("Hilmir")
+if kyn == ("strákur") and n2 == 144:
+    print ("Hilmar")
+if kyn == ("strákur") and n2 == 145:
+    print ("Hugi")
+if kyn == ("strákur") and n2 == 146:
+    print ("Henrik")
+if kyn == ("strákur") and n2 == 147:
+    print ("Hinrik")
+if kyn == ("strákur") and n2 == 148:
+    print ("Guðbjörn")
+if kyn == ("strákur") and n2 == 149:
+    print ("Guðni")
+if kyn == ("strákur") and n2 == 150:
+    print ("Gunnbjörn")
+
+if kyn == ("strákur") and n3 == 151:
+    print("Logi")
+if kyn == ("strákur") and n3 == 152:
+    print("Eyðar")
+if kyn == ("strákur") and n3 == 153:
+    print("Vikingur")
+if kyn == ("strákur") and n3 == 154:
+    print("Helgi")
+if kyn == ("strákur") and n3 == 155:
+    print("Dagur")
+if kyn == ("strákur") and n3 == 156:
+    print("Spói")
+if kyn == ("strákur") and n3 == 157:
+    print("Björn")
+if kyn == ("strákur") and n3 == 158:
+    print("Bjarmi")
+if kyn == ("strákur") and n3 == 159:
+    print("Hólmsteinn")
+if kyn == ("strákur") and n3 == 160:
+    print("Steinn")
+if kyn == ("strákur") and n3 == 161:
+    print("Kári")
+if kyn == ("strákur") and n3 == 162:
+    print("Birgir")
+if kyn == ("strákur") and n3 == 163:
+    print("Snær")
+if kyn == ("strákur") and n3 == 164:
+    print("Ágúst")
+if kyn == ("strákur") and n3 == 165:
+    print("Barðar")
+if kyn == ("strákur") and n3 == 166:
+    print("Benidikt")
+if kyn == ("strákur") and n3 == 167:
+    print("Gauti")
+if kyn == ("strákur") and n3 == 168:
+    print("Máni")
+if kyn == ("strákur") and n3 == 169:
+    print("Már")
+if kyn == ("strákur") and n3 == 170:
+    print("Breki")
+if kyn == ("strákur") and n3 == 171:
+    print("Smári")
+if kyn == ("strákur") and n3 == 172:
+    print("Elí")
+if kyn == ("strákur") and n3 == 173:
+    print("Axel")
+if kyn == ("strákur") and n3 == 174:
+    print("Freyr")
+if kyn == ("strákur") and n3 == 175:
+    print("Örn")
+if kyn == ("strákur") and n3 == 176:
+    print("Daði")
+if kyn == ("strákur") and n3 == 177:
+    print("Darri")
+if kyn == ("strákur") and n3 == 178:
+    print("Thor")
+if kyn == ("strákur") and n3 == 179:
+    print("Hugi")
+if kyn == ("strákur") and n3 == 180:
+    print("Ernir")
+if kyn == ("strákur") and n3 == 181:
+    print("Ægir")
+if kyn == ("strákur") and n3 == 182:
+    print("Vilberg")
+if kyn == ("strákur") and n3 == 183:
+    print("Blómkvist")
+if kyn == ("strákur") and n3 == 184:
+    print("Blær")
+if kyn == ("strákur") and n3 == 185:
+    print("Rúnar")
+if kyn == ("strákur") and n3 == 186:
+    print("Hrafn")
+if kyn == ("strákur") and n3 == 187:
+    print("Andri")
+if kyn == ("strákur") and n3 == 188:
+    print("Sigurður")
+if kyn == ("strákur") and n3 == 189:
+    print("Guðmundur")
+if kyn == ("strákur") and n3 == 190:
+    print("Trausti")
+if kyn == ("strákur") and n3 == 191:
+    print("Már")
+if kyn == ("strákur") and n3 == 192:
+    print("Snorri")
+if kyn == ("strákur") and n3 == 193:
+    print("Bald")
+if kyn == ("strákur") and n3 == 194:
+    print("Bergmann")
+if kyn == ("strákur") and n3 == 195:
+    print("Sjávarson")
+if kyn == ("strákur") and n3 == 196:
+    print("Sveinn")
+if kyn == ("strákur") and n3 == 197:
+    print("Servíetta")
+if kyn == ("strákur") and n3 == 198:
+    print("Ær")
+if kyn == ("strákur") and n3 == 199:
+    print("Bjarmi")
+if kyn == ("strákur") and n3 == 200:
+    print("Logi")
