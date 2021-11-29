d0704e9b8871c84d5d2a06461b823b51c84a9179
haukur <72574515+HaukurLogi@users.noreply.github.com>
2021-11-29 09:44:40 +0000

    Add files via upload

---
+++ cent-to-feet-and-inch.py
@@ -0,0 +1,13 @@
+print("Centimeter to Feet and Inches 5000")
+Centimeter = float(input("Centimeters:"))
+Feet = Centimeter / 30.48
+Inches = Feet / 12
+
+RoundFeet = round(Feet, 1)
+
+if Centimeter <= 0:
+    print("Invalid Number")
+if Inches > 12:
+    1 + Feet
+if Centimeter > 0:
+    print("You are about", RoundFeet, "feet tall.")
